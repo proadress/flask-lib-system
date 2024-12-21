4bf133e3c7e9c3fd460368d3aeb497f339f0091a
cloud <101621632+proadress@users.noreply.github.com>
2024-12-21 14:53:13 +0800

    0

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask_login import LoginManager
 from flask_cors import CORS
+
 # import sys
 # import os
 
@@ -17,9 +18,20 @@ app.config["SECRET_KEY"] = "ytfyfyt wajhfjawf"
 CORS(app, resources={r"/*": {"origins": "https://flask-lib-system.vercel.app/"}})
 
 
-app.register_blueprint(views, url_prefix="/")
+# app.register_blueprint(views, url_prefix="/")
 # app.register_blueprint(auth, url_prefix="/")
 # app.register_blueprint(admin, url_prefix="/admin")
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/about")
+def about():
+    return "About"
+
 
 login_manager = LoginManager()
 login_manager.login_view = "auth.login"
